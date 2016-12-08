02436a6111a850fcd41ecfd8e05ae86eb6126670
Tasha Casagni <casagni708@hotmail.com>
2016-12-07 19:25:59 -0600

    updated images

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,9 +4,9 @@
 // ==============================================================================
 
 var matches = [{
-  routeName: "johnmal",
-  name: "John Mal",
-  photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  routeName: "walterwhite",
+  name: "Walter White",
+  photo :"http://www.wow247.co.uk/wp-content/uploads/2014/08/Bryan-Cranston-breaking-bad.jpg",
   scores :[
      5,
      1,
@@ -20,9 +20,9 @@ var matches = [{
      1
    ]
 }, {
-  routeName: "hankmcgover",
-  name: "Hank McGover",
-  photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  routeName: "hankschrader",
+  name: "Hank Schrader",
+  photo :"http://images6.fanpop.com/image/photos/36800000/Hank-Schrader-Breaking-Bad-hank-schrader-36838913-400-535.jpg",
   scores :[
      1,
      1,
@@ -36,9 +36,9 @@ var matches = [{
      1
    ]
 }, {
-  routeName: "lukewalker",
-  name: "Luke Walker",
-  photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  routeName: "saulgoodman",
+  name: "Saul Goodman",
+  photo :"http://vignette1.wikia.nocookie.net/breakingbad/images/5/59/Season_4_-_Saul.jpg/revision/latest?cb=20110623000735",
   scores :[
      5,
      5,
