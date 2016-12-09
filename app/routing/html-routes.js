29cb24708f4c5eae2600899c37d543b22c7ee0e9
Tasha Casagni <casagni708@hotmail.com>
2016-12-09 12:29:47 -0600

    updated titles

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,16 +1,3 @@
-// // Routes
-// // =============================================================
-
-// // Basic route that sends the user first to the AJAX Page
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
-
-// app.get("/survey", function(req, res) {
-//   res.sendFile(path.join(__dirname, "survey.html"));
-// });
-
-
 // ===============================================================================
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
